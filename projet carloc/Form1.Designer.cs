4c916fdc0709c8c1717cbfe015fd83a35aaed2fb
marobin <robin.martin@ynov.com>
2025-03-03 09:08:00 +0100

    Ajoutez des fichiers projet.

---
+++ projet carloc/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace projet_carloc
+{
+    partial class F
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label6 = new Label();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            textBox6 = new TextBox();
+            label8 = new Label();
+            textBox7 = new TextBox();
+            label9 = new Label();
+            textBox8 = new TextBox();
+            button1 = new Button();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            groupBox1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox8);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(textBox7);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(textBox6);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(39, 37);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(1304, 543);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Enregistrer votre véhicule";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(386, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(542, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Veuillez entrer les informations de votre véhicule.";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(70, 133);
+            label2.Name = "label2";
+            label2.Size = new Size(109, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Marque :";
+            label2.Click += label2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(185, 133);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(200, 39);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(530, 133);
+            label3.Name = "label3";
+            label3.Size = new Size(108, 32);
+            label3.TabIndex = 3;
+            label3.Text = "Modèle :";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(644, 133);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(200, 39);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(958, 136);
+            label4.Name = "label4";
+            label4.Size = new Size(95, 32);
+            label4.TabIndex = 5;
+            label4.Text = "Année :";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(1059, 133);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(200, 39);
+            textBox3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(31, 250);
+            label5.Name = "label5";
+            label5.Size = new Size(148, 32);
+            label5.TabIndex = 7;
+            label5.Text = "Prix d'achat :";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(185, 247);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(200, 39);
+            textBox4.TabIndex = 8;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(448, 250);
+            label6.Name = "label6";
+            label6.Size = new Size(190, 32);
+            label6.TabIndex = 9;
+            label6.Text = "Prix de location :";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(644, 247);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(200, 39);
+            textBox5.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(987, 250);
+            label7.Name = "label7";
+            label7.Size = new Size(66, 32);
+            label7.TabIndex = 11;
+            label7.Text = "Etat :";
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(1059, 250);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(200, 39);
+            textBox6.TabIndex = 12;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(166, 361);
+            label8.Name = "label8";
+            label8.Size = new Size(155, 32);
+            label8.TabIndex = 13;
+            label8.Text = "Kilométrage :";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(327, 358);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(200, 39);
+            textBox7.TabIndex = 14;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(665, 361);
+            label9.Name = "label9";
+            label9.Size = new Size(131, 32);
+            label9.TabIndex = 15;
+            label9.Text = "Carburant :";
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(802, 358);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(200, 39);
+            textBox8.TabIndex = 16;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(482, 477);
+            button1.Name = "button1";
+            button1.Size = new Size(326, 46);
+            button1.TabIndex = 17;
+            button1.Text = "Enregistrement du véhicule";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1397, 973);
+            tabControl1.TabIndex = 18;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(groupBox1);
+            tabPage1.Location = new Point(8, 46);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1381, 919);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(8, 46);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(384, 146);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // F
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1411, 989);
+            Controls.Add(tabControl1);
+            Name = "F";
+            Text = "CarLoc accueil";
+            Load += F_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox6;
+        private Label label7;
+        private TextBox textBox5;
+        private Label label6;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox3;
+        private Label label4;
+        private Button button1;
+        private TextBox textBox8;
+        private Label label9;
+        private TextBox textBox7;
+        private Label label8;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
---
+++ projet carloc/Form1.cs
@@ -0,0 +1,20 @@
+namespace projet_carloc
+{
+    public partial class F : Form
+    {
+        public F()
+        {
+            InitializeComponent();
+        }
+
+        private void F_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
