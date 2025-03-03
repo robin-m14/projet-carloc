37b0d72f010dde949eb8cb64ad80f22d4fe38a08
ryanis <yanis.raes@ynov.com>
2025-03-03 09:41:09 +0100

    test

--- projet carloc/Form1.Designer.cs
+++ projet carloc/Form1.Designer.cs
@@ -29,243 +29,227 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            label1 = new Label();
-            label2 = new Label();
-            textBox1 = new TextBox();
+            button1 = new Button();
+            textBox3 = new TextBox();
             label3 = new Label();
             textBox2 = new TextBox();
-            label4 = new Label();
-            textBox3 = new TextBox();
-            label5 = new Label();
-            textBox4 = new TextBox();
-            label6 = new Label();
-            textBox5 = new TextBox();
-            label7 = new Label();
-            textBox6 = new TextBox();
-            label8 = new Label();
-            textBox7 = new TextBox();
-            label9 = new Label();
-            textBox8 = new TextBox();
-            button1 = new Button();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label1 = new Label();
             tabControl1 = new TabControl();
             tabPage1 = new TabPage();
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            groupBox2 = new GroupBox();
+            button2 = new Button();
+            textBox5 = new TextBox();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            label4 = new Label();
             tabPage2 = new TabPage();
             groupBox1.SuspendLayout();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
+            statusStrip1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
             // 
             groupBox1.Controls.Add(button1);
-            groupBox1.Controls.Add(textBox8);
-            groupBox1.Controls.Add(label9);
-            groupBox1.Controls.Add(textBox7);
-            groupBox1.Controls.Add(label8);
-            groupBox1.Controls.Add(textBox6);
-            groupBox1.Controls.Add(label7);
-            groupBox1.Controls.Add(textBox5);
-            groupBox1.Controls.Add(label6);
-            groupBox1.Controls.Add(textBox4);
-            groupBox1.Controls.Add(label5);
             groupBox1.Controls.Add(textBox3);
-            groupBox1.Controls.Add(label4);
-            groupBox1.Controls.Add(textBox2);
             groupBox1.Controls.Add(label3);
-            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(textBox2);
             groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(textBox1);
             groupBox1.Controls.Add(label1);
-            groupBox1.Location = new Point(39, 37);
+            groupBox1.Location = new Point(21, 17);
+            groupBox1.Margin = new Padding(2, 1, 2, 1);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(1304, 543);
+            groupBox1.Padding = new Padding(2, 1, 2, 1);
+            groupBox1.Size = new Size(702, 218);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
-            groupBox1.Text = "Enregistrer votre véhicule";
+            groupBox1.Text = "Inscription";
+            groupBox1.Enter += groupBox1_Enter;
             // 
-            // label1
+            // button1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(386, 57);
-            label1.Name = "label1";
-            label1.Size = new Size(542, 32);
-            label1.TabIndex = 0;
-            label1.Text = "Veuillez entrer les informations de votre véhicule.";
+            button1.Location = new Point(233, 185);
+            button1.Name = "button1";
+            button1.Size = new Size(207, 29);
+            button1.TabIndex = 6;
+            button1.Text = "S'inscrire";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
-            // label2
+            // textBox3
             // 
-            label2.AutoSize = true;
-            label2.Location = new Point(70, 133);
-            label2.Name = "label2";
-            label2.Size = new Size(109, 32);
-            label2.TabIndex = 1;
-            label2.Text = "Marque :";
-            label2.Click += label2_Click;
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(185, 133);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(200, 39);
-            textBox1.TabIndex = 2;
+            textBox3.Location = new Point(233, 145);
+            textBox3.Name = "textBox3";
+            textBox3.PasswordChar = '*';
+            textBox3.Size = new Size(207, 23);
+            textBox3.TabIndex = 5;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(530, 133);
+            label3.Location = new Point(299, 127);
             label3.Name = "label3";
-            label3.Size = new Size(108, 32);
-            label3.TabIndex = 3;
-            label3.Text = "Modèle :";
+            label3.Size = new Size(77, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Mot de passe";
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(644, 133);
+            textBox2.Location = new Point(233, 92);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(200, 39);
-            textBox2.TabIndex = 4;
+            textBox2.Size = new Size(207, 23);
+            textBox2.TabIndex = 3;
+            textBox2.TextChanged += textBox2_TextChanged;
             // 
-            // label4
+            // label2
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(958, 136);
-            label4.Name = "label4";
-            label4.Size = new Size(95, 32);
-            label4.TabIndex = 5;
-            label4.Text = "Année :";
+            label2.AutoSize = true;
+            label2.Location = new Point(316, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Email";
             // 
-            // textBox3
+            // textBox1
             // 
-            textBox3.Location = new Point(1059, 133);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(200, 39);
-            textBox3.TabIndex = 6;
+            textBox1.Location = new Point(233, 35);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(207, 23);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
             // 
-            // label5
+            // label1
             // 
-            label5.AutoSize = true;
-            label5.Location = new Point(31, 250);
-            label5.Name = "label5";
-            label5.Size = new Size(148, 32);
-            label5.TabIndex = 7;
-            label5.Text = "Prix d'achat :";
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(185, 247);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(200, 39);
-            textBox4.TabIndex = 8;
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(448, 250);
-            label6.Name = "label6";
-            label6.Size = new Size(190, 32);
-            label6.TabIndex = 9;
-            label6.Text = "Prix de location :";
-            // 
-            // textBox5
-            // 
-            textBox5.Location = new Point(644, 247);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(200, 39);
-            textBox5.TabIndex = 10;
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(987, 250);
-            label7.Name = "label7";
-            label7.Size = new Size(66, 32);
-            label7.TabIndex = 11;
-            label7.Text = "Etat :";
-            // 
-            // textBox6
-            // 
-            textBox6.Location = new Point(1059, 250);
-            textBox6.Name = "textBox6";
-            textBox6.Size = new Size(200, 39);
-            textBox6.TabIndex = 12;
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(166, 361);
-            label8.Name = "label8";
-            label8.Size = new Size(155, 32);
-            label8.TabIndex = 13;
-            label8.Text = "Kilométrage :";
-            // 
-            // textBox7
-            // 
-            textBox7.Location = new Point(327, 358);
-            textBox7.Name = "textBox7";
-            textBox7.Size = new Size(200, 39);
-            textBox7.TabIndex = 14;
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Location = new Point(665, 361);
-            label9.Name = "label9";
-            label9.Size = new Size(131, 32);
-            label9.TabIndex = 15;
-            label9.Text = "Carburant :";
-            // 
-            // textBox8
-            // 
-            textBox8.Location = new Point(802, 358);
-            textBox8.Name = "textBox8";
-            textBox8.Size = new Size(200, 39);
-            textBox8.TabIndex = 16;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(482, 477);
-            button1.Name = "button1";
-            button1.Size = new Size(326, 46);
-            button1.TabIndex = 17;
-            button1.Text = "Enregistrement du véhicule";
-            button1.UseVisualStyleBackColor = true;
+            label1.AutoSize = true;
+            label1.Location = new Point(289, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(99, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Nom d'utilisateur";
+            label1.Click += label1_Click;
             // 
             // tabControl1
             // 
             tabControl1.Controls.Add(tabPage1);
             tabControl1.Controls.Add(tabPage2);
-            tabControl1.Location = new Point(12, 12);
+            tabControl1.Location = new Point(6, 6);
+            tabControl1.Margin = new Padding(2, 1, 2, 1);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(1397, 973);
+            tabControl1.Size = new Size(752, 456);
             tabControl1.TabIndex = 18;
             // 
             // tabPage1
             // 
+            tabPage1.Controls.Add(statusStrip1);
+            tabPage1.Controls.Add(groupBox2);
             tabPage1.Controls.Add(groupBox1);
-            tabPage1.Location = new Point(8, 46);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Margin = new Padding(2, 1, 2, 1);
             tabPage1.Name = "tabPage1";
-            tabPage1.Padding = new Padding(3);
-            tabPage1.Size = new Size(1381, 919);
+            tabPage1.Padding = new Padding(2, 1, 2, 1);
+            tabPage1.Size = new Size(744, 428);
             tabPage1.TabIndex = 0;
-            tabPage1.Text = "tabPage1";
+            tabPage1.Text = "Inscription et Connexion";
             tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1 });
+            statusStrip1.Location = new Point(2, 405);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(740, 22);
+            statusStrip1.TabIndex = 2;
+            statusStrip1.Text = "Bienvenue :";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(68, 17);
+            toolStripStatusLabel1.Text = "Bienvenue :";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(button2);
+            groupBox2.Controls.Add(textBox5);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(textBox4);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Location = new Point(21, 239);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(702, 161);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Connexion";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(233, 122);
+            button2.Name = "button2";
+            button2.Size = new Size(207, 33);
+            button2.TabIndex = 4;
+            button2.Text = "Se connecter";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(233, 93);
+            textBox5.Name = "textBox5";
+            textBox5.PasswordChar = '*';
+            textBox5.Size = new Size(207, 23);
+            textBox5.TabIndex = 3;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(299, 75);
+            label5.Name = "label5";
+            label5.Size = new Size(77, 15);
+            label5.TabIndex = 2;
+            label5.Text = "Mot de passe";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(233, 37);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(207, 23);
+            textBox4.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(289, 19);
+            label4.Name = "label4";
+            label4.Size = new Size(99, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Nom d'utilisateur";
             // 
             // tabPage2
             // 
-            tabPage2.Location = new Point(8, 46);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Margin = new Padding(2, 1, 2, 1);
             tabPage2.Name = "tabPage2";
-            tabPage2.Padding = new Padding(3);
-            tabPage2.Size = new Size(384, 146);
+            tabPage2.Padding = new Padding(2, 1, 2, 1);
+            tabPage2.Size = new Size(744, 428);
             tabPage2.TabIndex = 1;
             tabPage2.Text = "tabPage2";
             tabPage2.UseVisualStyleBackColor = true;
             // 
             // F
             // 
-            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1411, 989);
+            ClientSize = new Size(760, 464);
             Controls.Add(tabControl1);
+            Margin = new Padding(2, 1, 2, 1);
             Name = "F";
             Text = "CarLoc accueil";
             Load += F_Load;
@@ -273,32 +257,34 @@
             groupBox1.PerformLayout();
             tabControl1.ResumeLayout(false);
             tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private GroupBox groupBox1;
-        private Label label2;
-        private Label label1;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Label label3;
-        private TextBox textBox6;
-        private Label label7;
-        private TextBox textBox5;
-        private Label label6;
-        private TextBox textBox4;
-        private Label label5;
-        private TextBox textBox3;
-        private Label label4;
-        private Button button1;
-        private TextBox textBox8;
-        private Label label9;
-        private TextBox textBox7;
-        private Label label8;
         private TabControl tabControl1;
         private TabPage tabPage1;
         private TabPage tabPage2;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox textBox3;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label2;
+        private GroupBox groupBox2;
+        private TextBox textBox5;
+        private Label label5;
+        private TextBox textBox4;
+        private Label label4;
+        private Button button2;
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
     }
 }
--- projet carloc/Form1.cs
+++ projet carloc/Form1.cs
@@ -1,7 +1,13 @@
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+
 namespace projet_carloc
 {
     public partial class F : Form
     {
+        string identifiant = "";
+        string email = "";
+        string motdepasse = "";
         public F()
         {
             InitializeComponent();
@@ -15,6 +21,91 @@ namespace projet_carloc
         private void label2_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            identifiant = textBox1.Text;
+            if (identifiant.Length < 4)
+            {
+                textBox2.Enabled = false;
+            }
+            else
+            {
+                textBox2.Enabled = true;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (email.Length <= 4)
+            {
+                textBox3.Enabled = true;
+            }
+            else
+            {
+                textBox3.Enabled = false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            identifiant = textBox1.Text;
+            email = textBox2.Text;
+            if (identifiant.Length <= 4)
+            {
+                MessageBox.Show("Vous n'avez pas le nombre de caractères requis pour votre identifiant");
+            }
+
+            bool EmailIsValid = false;
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                EmailIsValid = addr.Address == email;
+            }
+            catch
+            {
+                EmailIsValid = false;
+            }
+            if (!EmailIsValid)
+            {
+                MessageBox.Show("Veuillez entrer un email valide.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            motdepasse = textBox3.Text;
+            if (motdepasse.Length < 6 || !motdepasse.Any(char.IsUpper) || !motdepasse.Any(char.IsDigit))
+            {
+                MessageBox.Show("Le mot de passe doit contenir au moins 6 caractères, une majuscule et un chiffre.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            groupBox2.Enabled = true;
+            MessageBox.Show("Bienvenue " + identifiant + ", votre compte a bien été créé !", "Information", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox4.Text == identifiant && textBox5.Text == motdepasse)
+            {
+                toolStripStatusLabel1.Text = identifiant;
+                tabPage2.Enabled = true;
+                MessageBox.Show("Connexion réussi !", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Vérifiez vos informations !", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
